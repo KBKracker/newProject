fbbb14b987969187918e3ef24e91bc168843761c
Luca Casano <student8@js-c02mxpsdg083.punahou.edu>
2015-06-30 10:36:15 -1000

    Rock Paper Scissor Game

---
+++ app.js
@@ -0,0 +1,14 @@
+function var Choice1 = "computerChoice";
+var Choice1 = Math.floor() (Math.random()*3)
+if (Choice1 === 1)
+{
+	window.alert(Computer picked rock);
+}
+if (Choice1 === 2)
+{
+	window.alert(Computer picked paper);
+}
+if (Choice1 === 3)
+{
+	window.alert(Coputer picked paper);
+}
